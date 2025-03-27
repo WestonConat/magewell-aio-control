2dadad21b6c56ad9088d46ae969fb390d4743067
WestonConat <46689615+WestonConat@users.noreply.github.com>
2025-03-27 16:25:49 -0700

    Nav Menu + backend structure

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -2,13 +2,15 @@
 
 import Image from "next/image";
 import aio from "@/assets/AIO.png";
-import styles from "@/styles/header.module.css"
+import styles from "@/styles/Header.module.css"
+import NavMenu from "./NavMenu";
 
 export default function Header() {
     return (
         <header className={styles.headerWrapper}>
             <Image src={aio} alt="Magewell Device" width={200} height={200} />
-            <h1>Magewell Ultra Encode AIO Config Tool</h1>
+            {/* <h1>Magewell Ultra Encode AIO Config Tool</h1> */}
+            <NavMenu />
         </header>
     );
 }
---
+++ frontend/components/NavMenu.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Link from 'next/link';
+import styles from '@/styles/NavMenu.module.css';
+
+export default function NavMenu() {
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.menu}>
+        <li className={styles.menuItem}>
+          <Link href="/">Device Scanner</Link>
+        </li>
+        <li>|</li>
+        <li className={styles.menuItem}>
+          <Link href="/bulk-update">Bulk Update</Link>
+        </li>
+        {/* Add additional menu items here if needed */}
+      </ul>
+    </nav>
+  );
+}
